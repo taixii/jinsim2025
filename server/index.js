eeeda500008d6b3f94ee8fdeebe054b97b5adcfc
taixii <daniel1616@naver.com>
2025-06-10 14:19:14 +0000

    Feat: Express, MongoDB 연결을 위한 초기 세팅

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./db.js"; // 확장자 .js 반드시 붙이기
+
+dotenv.config();
+
+const app = express();
+const PORT = 4000;
+
+app.use(cors());
+app.use(express.json());
+
+await connectDB();
+
+app.get("/", (req, res) => {
+  res.send("서버 정상 작동 중");
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
