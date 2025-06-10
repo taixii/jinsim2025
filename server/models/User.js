c48904dbb6452032795d5a6960ca04dd7985ae96
taixii <daniel1616@naver.com>
2025-06-10 14:48:20 +0000

    Feat: User.js에서 유저 스키마 구체화

--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,40 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  phone: String,
-  role: { type: String, default: "user" },
+  role: {
+    type: String,
+    enum: ["student", "parent"],
+    required: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  school: {
+    type: String,
+    required: true
+  },
+  agreeMarketing: {
+    type: Boolean,
+    default: false
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  }
 });
 
 const User = mongoose.model("User", userSchema);
