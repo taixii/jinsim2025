c4a3b48e0acadf8c10e79a98bd54e68180bb5876
taixii <daniel1616@naver.com>
2025-06-29 14:41:41 +0000

    Style: Eliminate unnecessary border related codes in Footer & index.css

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <>
       <div class="double-hr"></div>
-      <footer className="text-gray-400 text-sm text-center p-4 border-t border-white">
+      <footer className="text-gray-400 text-sm text-center font-normal p-4">
         <p>대표자: 이태희 | 사업자등록번호: 648-04-02564</p>
         <p>© 2025 진심모의수능. All rights reserved.</p>
       </footer>
