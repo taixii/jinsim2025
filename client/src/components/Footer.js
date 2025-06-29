173d0b5c10ec9038e852873a9219e129a0c0ac26
taixii <daniel1616@naver.com>
2025-06-29 14:00:42 +0000

    Style: Put double-hr between Nav, Outlet, and Footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,12 @@
 function Footer() {
   return (
-    <footer className="text-gray-400 text-sm text-center p-4 border-t border-white">
-      <p>대표자: 이태희 | 사업자등록번호: 648-04-02564</p>
-      <p>© 2025 진심모의수능. All rights reserved.</p>
-    </footer>
+    <>
+      <div class="double-hr"></div>
+      <footer className="text-gray-400 text-sm text-center p-4 border-t border-white">
+        <p>대표자: 이태희 | 사업자등록번호: 648-04-02564</p>
+        <p>© 2025 진심모의수능. All rights reserved.</p>
+      </footer>
+    </>
   );
 }
 
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -2,19 +2,22 @@ import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav className="flex justify-between items-center p-4 border-b border-white">
-      <Link to="/" className="text-xl font-bold">진심모의수능</Link>
-      <div className="flex gap-4">
-        {['about', 'tests', 'search'].map((route, i) => (
-          <Link
-            key={i}
-            to={`/${route}`}
-          >
-            <button>{route === 'about' ? 'About' : route === 'tests' ? '신청하기' : '신청내역 확인'}</button>
-          </Link>
-        ))}
-      </div>
-    </nav>
+    <>
+      <nav className="flex justify-between items-center p-4">
+        <Link to="/" className="text-xl font-bold">진심모의수능</Link>
+        <div className="flex gap-4">
+          {['about', 'tests', 'search'].map((route, i) => (
+            <Link
+              key={i}
+              to={`/${route}`}
+            >
+              <button>{route === 'about' ? 'About' : route === 'tests' ? '신청하기' : '신청내역 확인'}</button>
+            </Link>
+          ))}
+        </div>
+      </nav>
+      <div class="double-hr"></div>
+    </>
   );
 }
 
